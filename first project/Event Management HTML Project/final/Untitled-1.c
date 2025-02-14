afeddca23eb39cb61aed2135d455bb8ce5727dd8
SanthoshMurugaesan2005 <santhoshmurugaesan1995@gmail.com>
2025-02-14 13:57:56 +0530

    commit

---
+++ first project/Event Management HTML Project/final/Untitled-1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    printf("Upcoming Events📅\n");
+    printf("Don't Miss any of your important events. Here are the important events you have registered for!\n");
+    printf("------------------------------------------------------------\n");
+
+    printf("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum scelerisque eros a lacinia. Cras nisl eros, tempor eu nisl nec, interdum ornare quam. Duis convallis, arcu in interdum consectetur, ipsum risus convallis tellus, et tincidunt sem ex quis libero. Fusce volutpat pellentesque nisi, in semper nunc elementum in. Suspendisse in varius tortor. Quisque in interdum massa, quis lacinia mauris. Cras scelerisque augue ac nibh malesuada, vel facilisis velit pharetra. Nullam aliquet diam eget ipsum malesuada auctor at non lorem. Donec nulla erat, efficitur id egestas non, ultricies sodales dui. Phasellus facilisis ex vitae velit volutpat, a rhoncus arcu tincidunt. Fusce maximus pharetra nisi eu euismod. Donec id velit mattis, sodales metus lacinia, finibus diam.\n");
+
+    printf("------------------------------------------------------------\n");
+    printf("Photo Gallery Event\n");
+    printf("Free Entry | Free Food | Pets are Not Allowed\n");
+    printf("The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.\n");
+
+    printf("------------------------------------------------------------\n");
+    printf("Fashion Store\n");
+    printf("Free Entry | Free Food | Pets are Not Allowed\n");
+    printf("The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.\n");
+
+    printf("------------------------------------------------------------\n");
+    printf("Contact\n");
+    printf("+91 787483783743 | eventcollab@gmail.com\n");
+    printf("9th street Avenue Park, Trichy, 625620\n");
+
+    return 0;
+}
